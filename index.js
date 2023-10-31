84a960cf5c25ad5c337fb8bcd92c5fc3b3cbc63f
Vishal <vishal.manglani7@gmail.com>
2023-10-31 14:17:56 -0400

    updated JS for viewallemployees and addemployee

--- index.js
+++ index.js
@@ -13,40 +13,102 @@ inquirer
         {type: "list",
         name:"choice",
         choices:[
-            "choice 1",
-            "choice 2",
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Quit",
         ]}
     ])
     .then((answers)=>{
-        //viewall employees()
         console.log("answers: ", answers)
-        if(answers.choice === "choice 1"){
-            viewAllEmployees()
+        if(answers.choice === "View All Employees"){
+            viewAllEmployees();
         }
-    })
-
+        if(answers.choice === "Add Employee"){
+            addEmployee();
+        }
+        if(answers.choice === "Update Employee Role"){
+            updateEmployeeRole();
+        }
+        if(answers.choice === "Add Role"){
+            addRole();
+        }
+        if(answers.choice === "View All Departments"){
+            viewAllDepartments();
+        }
+        if(answers.choice === "Quit"){
+            quit();}
+    });}
 //prompts for main menu fo here
 
 //if statements viewallempyoees()
 //unless user selects quit
 
-}
-
 const viewAllEmployees = () => {
     console.log("viewing all employees")
     con.connect(function(err) {
         if (err) throw err;
-        con.query("SELECT* FROM department", function (err, result, fields) {
+        con.query("SELECT* FROM employee", function (err, result) {
           if (err) throw err;
           console.log(result);
-
-
-
           homeMenu()
         });
       });
 }
 
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'Enter employee first name:'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'Enter employee last name:'
+        },
+        {
+            type: "list",
+            name: "choice",
+            message: "Please select Role ID:",
+            choices: [
+                1,
+                2,
+                3,
+                4,]
+        },
+        {
+            type: "list",
+            name: "choice",
+            message: "Please select Manager ID:",
+            choices: [
+                1,
+                2,
+                3,
+                4,
+                null,]
+        },
+    ])
+    .then(answers => {
+        const sql = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+        const values = [answers.firstName, answers.lastName, answers.roleId, answers.managerId];
+
+        con.connect(function(err) {
+            if (err) throw err;
+            con.query(sql, values, function (err, result) {
+                if (err) throw err;
+                homeMenu();
+            });});
+    });
+
+
+
+
+}
 
 
 
@@ -59,7 +121,6 @@ const viewAllEmployees = () => {
 
 
 
-homeMenu();
 
 // con.query (use this to display user request)
 //view all employees function()
